391ba529c502999d26bf57e308b535bdc35351de
codyduong <duongcody507@gmail.com>
2021-03-21 12:18:32 -0500

    RepoFlex width fix

--- src/code/RepoFlex.js
+++ src/code/RepoFlex.js
@@ -8,39 +8,32 @@ const RepoFlex = (props) => {
     flexDirection: "row"
   }
 
-  const repoInner = {
-    width: "100%",
-    height: "100%",
-    borderRadius: "25px",
-    border: "2px solid #777777",
-    padding: "10px"
-  }
-
   const repositoryFlex = props.repoData.map((row, index) => {
     const order = require('./Config.json').Whitelist[row.name] ?? null
     const display = require('./Config.json').Whitelist[row.name] ? "flex" : null
 
     const repoOuter = {
-      width: "calc(100% - 3.25%)",
+      width: "100%",
+      borderRadius: "25px",
+      border: "2px solid #777777",
       height: "100%",
       justifyContent: "center",
       alignItems: "center",
-      padding: "5px 0px 5px 0px",
+      padding: "10px",
+      margin: "5px 0px 5px 0px",
       order: order
     }
     return display ? (
       <div style={repoOuter} key={index}>
-        <div style={repoInner}>
-          <h1><a href={row.html_url} title="Github Repository">{row.name}</a></h1>
-          {row.description}
-          <br></br>
-          <br></br>
-          <div style={{width: '100%', marginLeft:"auto", marginRight:"auto"}}>
-            <LangBar 
-              languages = {row.languages ?? null} //Only used when RepoFetch fails, and defaults to FallbackRepos (in config) or Repos.js
-              languages_url={row.languages_url} 
-            />
-          </div>
+        <h1><a href={row.html_url} title="Github Repository">{row.name}</a></h1>
+        {row.description}
+        <br></br>
+        <br></br>
+        <div style={{ width: '100%', marginLeft: "auto", marginRight: "auto" }}>
+          <LangBar
+            languages={row.languages ?? null} //Only used when RepoFetch fails, and defaults to FallbackRepos (in config) or Repos.js
+            languages_url={row.languages_url}
+          />
         </div>
       </div>
     ) : null
